7341f717ae4909838e13c50ccff219183eed49a1
Hamza Iddou <hamzaiddou2004@gmail.com>
2026-03-11 01:53:36 +0000

    i forgot email vrfied column

--- Backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ Backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->boolean("is_email_verified")->default(false);
             $table->string('password');
             $table->timestamps();
         });
