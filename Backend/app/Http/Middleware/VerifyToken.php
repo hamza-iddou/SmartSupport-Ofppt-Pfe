0b51579d17d79c2979991899665c6f28cb4847db
Hamza Iddou <hamzaiddou2004@gmail.com>
2026-03-14 02:42:14 +0000

    make the middleware for refrsh and logout

---
+++ Backend/app/Http/Middleware/VerifyToken.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Exception;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\JWT;
+
+class VerifyToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        try{
+            $user = JWTAuth::parsetoken()->authenticate();
+            if($user){
+                return $next($request);
+            }            
+        }catch (TokenExpiredException $e) {
+            return response()->json([
+                'success' => false,
+                'msg' => 'Token has expired'
+            ], 401);
+            
+        } catch (TokenInvalidException $e) {
+            return response()->json([
+                'success' => false,
+                'msg' => 'Token is invalid'
+            ], 401);
+            
+        } catch (JWTException $e) {
+            return response()->json([
+                'success' => false,
+                'msg' => 'Token not provided or malformed'
+            ], 401);
+        }
+        
+    }
+}
--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,13 +1,21 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\API\LoginController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 
-Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/register', [AuthController::class, 'register'])->name('register');
-Route::post('/logout',[AuthController::class, 'logout'])->name('logout');
-Route::post('/refresh', [AuthController::class, 'refresh'])->name('refresh');
+// Route::get('/test', function () {
+//     return response()->json(['message' => 'API is working!']);
+// });
+
+Route::group(['namespace' => 'API'], function () {
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::group(["middleware" => "verify.token"], function () {
+        Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+        Route::post('/refresh', [AuthController::class, 'refresh'])->name('refresh');        
+    });
+});
